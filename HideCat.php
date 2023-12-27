f7643feb66df4e72489a5ad4fd3d12e174c176f5
jestincybrosys <jestin@cybrosys.in>
2023-12-27 11:08:37 +0530

    Update HideCat.php

--- HideCat.php
+++ HideCat.php
@@ -8,30 +8,25 @@ Author: Jestin Joseph
 Author URI: https://jestinjoseph.netlify.app/
 */
 // Check WooCommerce installed, activated, and compatible version
-function WC_visibility_version_check($version = '3.0') {
+function wchidecate_version_check($version = '3.0') {
     if (!class_exists('WooCommerce')) {
-        ?>
-        <div class="error notice">
-            <p><?php echo 'WooCommerce not found. Please install and activate it to use HideCat: Hide out of stock products by category.'; ?></p>
-        </div>
-        <?php
+        echo '<div class="error notice">';
+        echo '<p>WooCommerce not found. Please install and activate it to use HideCat: Hide out of stock products by category.</p>';
+        echo '</div>';
         return;
     }
 
     if (version_compare(WC_VERSION, $version, "<")) {
-        ?>
-        <div class="error notice">
-            <p><?php echo 'WooCommerce version too low! HideCat: Hide out of stock products by category requires version 3.0 or greater.'; ?></p>
-        </div>
-        <?php
+        echo '<div class="error notice">';
+        echo '<p>WooCommerce version too low! HideCat: Hide out of stock products by category requires version 3.0 or greater.</p>';
+        echo '</div>';
         return;
     }
 }
-add_action('init', 'WC_visibility_version_check');
+add_action('init', 'wchidecate_version_check');
 
 // Change the query to exclude products that are out of stock and have the categories to be hidden
-function WC_visibility_hide_outofstock_products($q) {
-
+function wchidecate_hide_outofstock_products($q) {
     if (!$q->is_main_query() || is_admin()) {
         return;
     }
@@ -101,13 +96,12 @@ function WC_visibility_hide_outofstock_products($q) {
     // Remove them from the main query
     $q->set('post__not_in', $overlap);
 
-    remove_action('pre_get_posts', 'WC_visibility_hide_outofstock_products');
-
+    remove_action('pre_get_posts', 'wchidecate_hide_outofstock_products');
 }
-add_action('pre_get_posts', 'WC_visibility_hide_outofstock_products');
+add_action('pre_get_posts', 'wchidecate_hide_outofstock_products');
 
 // Add to new term page
-function WC_visibility_added_hide_in_cat($taxonomy) {
+function wchidecate_added_hide_in_cat($taxonomy) {
     $hide_all_setting = get_option('woocommerce_hide_out_of_stock_items', true);
     $checkbox_output = '<input type="checkbox" id="hide_products_in_cat" name="hide_products_in_cat" value="yes" />';
     if ($hide_all_setting == 'yes') {
@@ -121,10 +115,10 @@ function WC_visibility_added_hide_in_cat($taxonomy) {
     </div>
     <?php
 }
-add_action('product_cat_add_form_fields', 'WC_visibility_added_hide_in_cat', 99, 2);
+add_action('product_cat_add_form_fields', 'wchidecate_added_hide_in_cat', 99, 2);
 
 // Add to edit term page
-function WC_visibility_edited_hide_in_cat($term, $taxonomy) {
+function wchidecate_edited_hide_in_cat($term, $taxonomy) {
     $hide_products_in_cat = get_term_meta($term->term_id, 'hide_products_in_cat', true);
 
     $hide_all_setting = get_option('woocommerce_hide_out_of_stock_items', true);
@@ -140,15 +134,14 @@ function WC_visibility_edited_hide_in_cat($term, $taxonomy) {
         </th>
         <td>
             <?php echo $checkbox_output; ?><p>You can also go to category settings to change this option</p>
-
         </td>
     </tr>
     <?php
 }
-add_action('product_cat_edit_form_fields', 'WC_visibility_edited_hide_in_cat', 99, 2);
+add_action('product_cat_edit_form_fields', 'wchidecate_edited_hide_in_cat', 99, 2);
 
 // Save category settings
-function WC_visibility_save_hide_in_cat($term_id, $tag_id) {
+function wchidecate_save_hide_in_cat($term_id, $tag_id) {
     $setting = sanitize_key($_POST['hide_products_in_cat']);
     if (!empty($setting)) {
         update_term_meta($term_id, 'hide_products_in_cat', 'yes');
@@ -156,24 +149,24 @@ function WC_visibility_save_hide_in_cat($term_id, $tag_id) {
         delete_term_meta($term_id, 'hide_products_in_cat');
     }
 }
-add_action('created_product_cat', 'WC_visibility_save_hide_in_cat', 10, 2);
-add_action('edited_product_cat', 'WC_visibility_save_hide_in_cat', 10, 2);
+add_action('created_product_cat', 'wchidecate_save_hide_in_cat', 10, 2);
+add_action('edited_product_cat', 'wchidecate_save_hide_in_cat', 10, 2);
 
 // Add a custom menu in the WordPress admin sidebar
-function WC_visibility_category_menu() {
+function wchidecate_category_menu() {
     add_menu_page(
         'Category Settings',
         'Category Settings',
         'manage_options',
         'category-settings',
-        'WC_visibility_category_settings_page',
+        'wchidecate_category_settings_page',
         'dashicons-category',
         57
     );
 }
-add_action('admin_menu', 'WC_visibility_category_menu');
+add_action('admin_menu', 'wchidecate_category_menu');
 
-function WC_visibility_category_settings_page() {
+function wchidecate_category_settings_page() {
     if (isset($_POST['category_settings'])) {
         // Handle form submission
         $category_settings = isset($_POST['category_settings']) ? $_POST['category_settings'] : array();
@@ -216,11 +209,10 @@ function WC_visibility_category_settings_page() {
     <?php
 }
 
-add_action('admin_post_save_category_settings', 'WC_visibility_category_settings_page');
-
+add_action('admin_post_save_category_settings', 'wchidecate_category_settings_page');
 
 // Save category settings
-function WC_visibility_save_category_settings() {
+function wchidecate_save_category_settings() {
     if (isset($_POST['category_settings']) && is_array($_POST['category_settings'])) {
         foreach ($_POST['category_settings'] as $category_id => $setting) {
             $setting = sanitize_key($setting);
@@ -234,7 +226,7 @@ function WC_visibility_save_category_settings() {
 }
 
 // Save category settings for the custom sidebar menu
-function WC_visibility_save_category_settings_sidebar() {
+function wchidecate_save_category_settings_sidebar() {
     if (isset($_POST['category_settings_sidebar']) && is_array($_POST['category_settings_sidebar'])) {
         foreach ($_POST['category_settings_sidebar'] as $category_id => $setting) {
             $setting = sanitize_key($setting);
@@ -246,18 +238,18 @@ function WC_visibility_save_category_settings_sidebar() {
         }
     }
 }
-add_action('admin_post_save_category_settings_sidebar', 'WC_visibility_save_category_settings_sidebar');
+add_action('admin_post_save_category_settings_sidebar', 'wchidecate_save_category_settings_sidebar');
 
 // Hook into the admin action to handle the form submission
 add_action('admin_init', function () {
     if (isset($_POST['_wpnonce']) && isset($_POST['action']) && ($_POST['action'] === 'save_category_settings' || $_POST['action'] === 'save_category_settings_sidebar')) {
-        WC_visibility_save_category_settings();
-        WC_visibility_save_category_settings_sidebar();
+        wchidecate_save_category_settings();
+        wchidecate_save_category_settings_sidebar();
     }
 });
 
 // Enqueue styles for the custom admin page
-function WC_visibility_enqueue_admin_styles() {
+function wchidecate_enqueue_admin_styles() {
     if (isset($_GET['page']) && ($_GET['page'] === 'category-settings' || $_GET['page'] === 'category-sidebar-settings')) {
         wp_enqueue_style('wp-admin');
     }
@@ -266,4 +258,4 @@ function WC_visibility_enqueue_admin_styles() {
     wp_enqueue_style('chosen-css', 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css', array(), '1.8.7');
     wp_enqueue_style('filter-css', plugin_dir_url(__FILE__) . 'css/filter.css');
 }
-add_action('admin_enqueue_scripts', 'WC_visibility_enqueue_admin_styles');
+add_action('admin_enqueue_scripts', 'wchidecate_enqueue_admin_styles');
